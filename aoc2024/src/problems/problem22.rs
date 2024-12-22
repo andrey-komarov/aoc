d8616053c9fb46fc550f6a662b58aeb7be5b014d
Andrey Komarov <akmrv@pm.me>
2024-12-22 12:53:22 +0000

    Solved 22/part2

--- aoc2024/src/problems/problem22.rs
+++ aoc2024/src/problems/problem22.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::io::{BufRead, Write};
 use crate::problems::common::{Readable, Solvable};
 
@@ -72,6 +73,64 @@ impl Solvable for PartOne {
     }
 }
 
+impl PartTwo {
+    pub(crate) fn new(steps: usize) -> Self {
+        Self { steps }
+    }
+
+    fn bananas(num: u64) -> i32 {
+        (num % 10) as i32
+        // format!("{}", num).chars().filter(|c| *c == '1').count() as i32
+    }
+
+    fn changes_and_prices(&self, secret: u64) -> Vec<(i32, i32)> {
+        let mut prices = Vec::new();
+        let mut prng = PRNG::new(secret);
+        let mut x = secret;
+        for _ in 0..=self.steps {
+            prices.push(Self::bananas(x));
+            x = prng.next();
+        }
+        prices.iter().skip(1).zip(prices.iter()).map(|(a, b)| {
+            *a as i32 - *b as i32
+        }).zip(prices.iter().skip(1)).map(|(change, price)| (change, *price)).collect()
+    }
+
+    fn solve(&self, input: &Input) -> Output {
+        let mut total = HashMap::new();
+        for secret in input.secrets.iter() {
+            let mut seen = HashSet::new();
+            let x =  self.changes_and_prices(*secret);
+            let i1 = x.iter();
+            let i2 = x.iter().skip(1);
+            let i3 = x.iter().skip(2);
+            let i4 = x.iter().skip(3);
+            i1.zip(i2).zip(i3).zip(i4).for_each(|(((v1, v2), v3), v4)| {
+                let quad = (v1.0, v2.0, v3.0, v4.0);
+                let price = v4.1;
+                if !seen.contains(&quad) {
+                    seen.insert(quad);
+                    *total.entry(quad).or_insert(0) += price;
+                }
+            })
+        }
+        *total.values().max().unwrap() as Output
+    }
+}
+
+pub(crate) struct PartTwo {
+    steps: usize,
+}
+
+impl Solvable for PartTwo {
+    fn solve<R: BufRead, W: Write>(&self, input: R, mut output: W) -> anyhow::Result<()> {
+        let input = Input::parse_from(input)?;
+        let out = self.solve(&input);
+        writeln!(output, "{}", out)?;
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::problems::problem22::*;
@@ -81,5 +140,13 @@ mod tests {
         let mut prng = PRNG::new(123);
         let next = prng.next();
         assert_eq!(next, 15887950);
+    }
+
+    #[test]
+    fn test_part2() {
+        let p2 = PartTwo::new(10);
+        let c = p2.changes_and_prices(123);
+        println!("{:?}", c);
+        assert_eq!(1, 1);
     }
 }
